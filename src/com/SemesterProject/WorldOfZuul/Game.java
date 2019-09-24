a1d7b4d8298d9408d37679c38d03ef6cb9888ab0
lucas sass <lucassass@live.com>
2019-09-24 10:55:59 +0200

    hotfix

--- src/com/SemesterProject/WorldOfZuul/Game.java
+++ src/com/SemesterProject/WorldOfZuul/Game.java
@@ -5,12 +5,6 @@ public class Game
     private Parser parser;
     private Room currentRoom;
     
-// To Start game run main function bellow
-public static void main(String[] args) {
-    Game game = new Game();
-    game.play();
-}
-
     /*
     Create the game and initialise its internal map.
     */
