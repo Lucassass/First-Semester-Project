a79792574c89ed9cf0c4fe7d59832543430abc21
telmerie <tes_7583@hotmail.com>
2019-10-16 14:29:05 +0200

    Create Config.java

---
+++ src/com/SemesterProject/WorldOfZuul/Config.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.SemesterProject.WorldOfZuul;
+
+import java.util.ArrayList;
+
+
+public class Config
+{
+    private static int money;
+    public static int sustainability = 0;
+    public static int energy = 0;
+    public static int environment = 0;
+    public static int diceAdvantage = 0;
+
+  /* public void updateStats()
+   {
+       Inventory inventory = new Inventory();
+              
+       for(int i = 0; i < inventory.getInventory().size(); i++)
+       {
+           inventory.getInventory().get(i).
+           diceAdvantage = diceAdvantage + pointsGood + pointsBad;
+       }
+   }
+    
+    public static int getMoney() {
+        return money;
+    }
+
+    public static void setMoney(int newMoney) {
+        money = newMoney;
+    }*/
+}
