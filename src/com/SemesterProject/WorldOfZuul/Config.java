f60c64a35fdc2c73b9bb28216c2ec2bf23018c30
telmerie <tes_7583@hotmail.com>
2019-10-16 23:06:26 +0200

    Update Config.java

--- src/com/SemesterProject/WorldOfZuul/Config.java
+++ src/com/SemesterProject/WorldOfZuul/Config.java
@@ -6,25 +6,56 @@
 package com.SemesterProject.WorldOfZuul;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
-public class Config
+public class Config extends Inventory
 {
+    private static String dansk = "Danmark"; //slettes når opdatere rollDice
     private static int money;
     public static int sustainability = 0;
     public static int energy = 0;
     public static int environment = 0;
-    public static int diceAdvantage = 0;
+    private static int diceAdvantage = 0;
+    private static int diceResult = 0;
+    
+    Inventory inventory = new Inventory();
 
-  /* public void updateStats()
+    /**
+     * Needs to replace the dansk part with objekt country.
+     * when player rolls dice go through inventory and chek the advantages and
+     * disadvantages player gets from items depending on the country. 
+     * Then roll dice, if between 4 and 7 return true else return false.
+     */
+   public boolean rollDice()
    {
-       Inventory inventory = new Inventory();
-              
        for(int i = 0; i < inventory.getInventory().size(); i++)
        {
-           inventory.getInventory().get(i).
-           diceAdvantage = diceAdvantage + pointsGood + pointsBad;
+         if(inventory.getInventory().get(i).getCountryGood(this).equals(dansk) )
+         {
+             diceAdvantage = inventory.getInventory().get(i).getPointsGood(this);
+         }
+         else if(inventory.getInventory().get(i).getcountryBad(this).equals(dansk) )
+         {
+             diceAdvantage = inventory.getInventory().get(i).getPointsBad(this);
+         }else if(inventory.getInventory().get(i).getCountryGood(this).equals(dansk) 
+                 && inventory.getInventory().get(i).getcountryBad(this).equals(dansk))
+         {
+             diceAdvantage = inventory.getInventory().get(i).getPointsGood(this) 
+                     + inventory.getInventory().get(i).getPointsBad(this);
+         }
+         
+         diceResult = new Random().nextInt(7) + 1 + diceAdvantage;
+             
        }
+       
+       if (3 < diceResult < 7)
+       {
+           return true;
+       } else {
+           return false;
+       }
+       
    }
     
     public static int getMoney() {
@@ -33,5 +64,5 @@ public class Config
 
     public static void setMoney(int newMoney) {
         money = newMoney;
-    }*/
+    }
 }
