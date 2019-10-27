7645b53c04e197eb2ffc64292052bf18dd5af1e7
telmerie <tes_7583@hotmail.com>
2019-10-27 21:31:18 +0100

    Update Deal.java

--- src/com/SemesterProject/WorldOfZuul/Deal.java
+++ src/com/SemesterProject/WorldOfZuul/Deal.java
@@ -11,4 +11,71 @@ package com.SemesterProject.WorldOfZuul;
  */
 public class Deal {
     
+    static String name; // Name of deal
+    static String category; //Catogery of deals
+    static int sustainabilityPoints; // effects on sustainability
+    static int energyPoints; //effect on energy
+    static int environmentPoints; // effect on environment
+    static String information; //Information about deal
+    
+    
+    public Deal()
+    {
+        name = null; category = null; sustainabilityPoints = 0; 
+        energyPoints = 0; environmentPoints = 0; information = null;
+    }
+    //Hoved Deal objektet der gerne skulle bruges
+    public Deal(String name, String category, int sustainabilityPoints, 
+            int energyPoints, int environmentPoints)
+    {
+        name = Deal.name;
+        category = Deal.category;
+        sustainabilityPoints = Deal.sustainabilityPoints;
+        energyPoints = Deal.energyPoints;
+        environmentPoints = Deal.environmentPoints;
+        String info = information; 
+    }
+    /**
+     * 
+     * returns information
+     * @return 
+     */
+    public static String getInfo(){return information;}
+    /**
+     * sets information about each deal
+     * @param info 
+     */
+    public static void setInfo(String info){info = getInfo();}
+    
+    /**
+     *    
+     * @return 
+     */
+    public static String getName(){return name;}
+    
+    /**
+     * category
+     * @return 
+     */
+    public static String getCategory(){return category;}
+    /**
+     * 
+     * @param deal
+     * @return 
+     */
+    public static int getSusPoints(Deal deal){return sustainabilityPoints;}
+    /**
+     * 
+     * @param deal
+     * @return 
+     */
+    public static int getEnergyPoints(Deal deal){return energyPoints;}
+    /**
+     * 
+     * @param deal
+     * @return 
+     */
+    public static int getEnvPoints(Deal deal){return environmentPoints;}
+    
+    
 }
